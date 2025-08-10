92dd25869e7c860e1803e1c58c0ee9e837ec61c3
AKTECHLEARN <AKTECHLEARN@GMAIL.COM>
2025-08-11 00:38:09 +0530

    Update style.js

--- game/style.js
+++ game/style.js
@@ -4,33 +4,48 @@ let turn = "X";
 
 function checkWinner() {
     const winPatterns = [
-        [0,1,2], [3,4,5], [6,7,8],
-        [0,3,6], [1,4,7], [2,5,8],
-        [0,4,8], [2,4,6]
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
     ];
+
     for (let pattern of winPatterns) {
         const [a, b, c] = pattern;
-        if (cells[a].innerText &&
+        if (
+            cells[a].innerText &&
             cells[a].innerText === cells[b].innerText &&
-            cells[a].innerText === cells[c].innerText) {
+            cells[a].innerText === cells[c].innerText
+        ) {
             alert(`${cells[a].innerText} wins!`);
-            location.reload();
+            resetBoard();
+            return;
         }
     }
+
+    if (cells.every(cell => cell.innerText !== "")) {
+        alert("It's a draw!");
+        resetBoard();
+    }
+}
+
+function resetBoard() {
+    cells.forEach(cell => cell.innerText = "");
+    turn = "X";
 }
 
 function handleClick(e) {
-    if (e.target.innerText === "") {
+    if (!e.target.innerText) {
         e.target.innerText = turn;
-        turn = turn === "X" ? "O" : "X";
         checkWinner();
+        turn = turn === "X" ? "O" : "X";
     }
 }
 
+// Create game cells
 for (let i = 0; i < 9; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     cell.addEventListener("click", handleClick);
     board.appendChild(cell);
     cells.push(cell);
-} 
+}
